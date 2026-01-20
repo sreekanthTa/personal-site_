b42fc79929c4edc5d0aebf62151157ada2bfeeca
sreekanthTa <sreekanthsreekantht970@gamil.com>
2026-01-20 03:59:25 +0000

    add test case

--- __tests__/page.test.js
+++ __tests__/page.test.js
@@ -1,13 +1,28 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Page from '../app/page'
+import userEvent from "@testing-library/user-event";
 
+
+jest.mock('../actions/redirect', () => ({
+    navigateToNotes: jest.fn(),
+}))
+
+import { navigateToNotes } from '../actions/redirect'
 
 describe('Home Page testing', ()=>{
     it('render Learning Notes',()=>{
         render(<Page/>)
         const headingElement = screen.getByText(/LEARNING NOTES/i)
         expect(headingElement).toBeInTheDocument()
-                               
     })
+
+    it('div has onClick handler', async ()=>{
+
+        render(<Page/>)
+        const divElement = screen.getByTestId("page-container")
+        await userEvent.click(divElement)
+        expect(navigateToNotes).toHaveBeenCalled()
+    })
+
 })
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import {navigateToNotes} from "./actions/redirect";
+import {navigateToNotes} from "../actions/redirect";
 
 export default  function Page() {
   
   return (
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", flexDirection:"column", gap:"1rem", background:"black"}}
+    <button data-testid="page-container" style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", flexDirection:"column", gap:"1rem", background:"black"}}
     onClick={navigateToNotes}
     >
        <h1 style={{color:"white", fontSize:"min(10vw, 10vh)"}}> LEARNING NOTES</h1>
-    </div>
+    </button>
   );
-}
+} 
 
